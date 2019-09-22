d2b08c2d8c3897342e6ca41defaeb19e51284fec
Gabriel Petry <gabrielpetry@outlook.com>
2019-09-22 11:28:14 -0300

    Added a prefix to routes

--- src/routes.js
+++ src/routes.js
@@ -7,21 +7,23 @@ const UserController = require('./controllers/user')
 const GuildController = require('./controllers/guild')
 const routes = express.Router();
 
+const VERSION = '';
+
 // users routse
-routes.post('/users/', UserController.store);
-routes.post('/users/auth', UserController.auth);
+routes.post(`${VERSION}/users/`, UserController.store);
+routes.post(`${VERSION}/users/auth`, UserController.auth);
 // guild routes
-routes.get('/guilds', authMiddleware, GuildController.list);
-routes.post('/guilds', authMiddleware, GuildController.store);
+routes.get(`${VERSION}/guilds`, authMiddleware, GuildController.list);
+routes.post(`${VERSION}/guilds`, authMiddleware, GuildController.store);
 // playlists routes
-routes.get('/playlists/songs', authMiddleware, PlaylistController.getWithSongs);
-routes.get('/playlists', authMiddleware, PlaylistController.index);
-routes.post('/playlists', authMiddleware, PlaylistController.store);
-// routes.delete('/playlists', authMiddleware, PlaylistController.remove);
+routes.get(`${VERSION}/playlists/:playlist`, authMiddleware, PlaylistController.PlaylistSongs);
+routes.get(`${VERSION}/playlists`, authMiddleware, PlaylistController.index);
+routes.post(`${VERSION}/playlists`, authMiddleware, PlaylistController.store);
+// routes.delete(`${VERSION}/playlists`, authMiddleware, PlaylistController.remove);
 // songs routes
-routes.get('/songs', authMiddleware, SongController.index);
-routes.get('/songs/:playlist', authMiddleware, SongController.get);
-routes.post('/songs', authMiddleware, SongController.store);
-// routes.delete('/songs', authMiddleware, SongController.remove);
+routes.get(`${VERSION}/songs`, authMiddleware, SongController.index);
+// routes.get(`${VERSION}/songs/:playlist`, authMiddleware, SongController.get);
+routes.post(`${VERSION}/songs`, authMiddleware, SongController.store);
+// routes.delete(`${VERSION}/songs`, authMiddleware, SongController.remove);
 
 module.exports = routes;
