2d608f7174ab39ab4aa1749aaf65d50985ada546
Gabriel Petry <gabrielpetry@outlook.com>
2019-10-29 18:10:24 -0300

    prettier

--- src/database.js
+++ src/database.js
@@ -1,18 +1,12 @@
 const mongoose = require('mongoose')
-const { db_host,
-        db_user,
-        db_password,
-        db_database } = require('./config.json');
-
+const { db_host, db_user, db_password, db_database } = require('./config.json')
 
 console.log(db_password)
-const db = mongoose.connect(
-  `mongodb://${db_host}:27017/${db_database}`, {
-    user: db_user,
-    pass: db_password,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  }
-);
-module.exports = db;
+const db = mongoose.connect(`mongodb://${db_host}:27017/${db_database}`, {
+  user: db_user,
+  pass: db_password,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+module.exports = db
