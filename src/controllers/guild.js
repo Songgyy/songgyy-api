d7402f41aa14e4046764d7a8349a9d3f36159507
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-03 11:14:47 -0300

    better validations

--- src/controllers/guild.js
+++ src/controllers/guild.js
@@ -1,42 +1,36 @@
 const Guild = require('../model/Guild')
 const User = require('../model/User')
 
-
 module.exports = {
   async raiseHttpError(res, message, status = 400) {
-      return res.status(status).send({ error: message });
+    return res.status(status).send({ error: message })
   },
 
   async list(req, res) {
-
     const { guilds } = await User.findById(req.user.id).populate('guilds')
-    res.send({ guilds });
+    res.send({ guilds })
   },
 
   async store(req, res) {
     const self = module.exports
-   
-    if (!req.body.name)
-      return self.raiseHttpError(res, `No guild name provided provided`);
 
-    if (!req.body.guild_id)
-      return self.raiseHttpError(res, `No guild_id provided`);
+    if (!req.body.name) return self.raiseHttpError(res, `No guild name provided provided`)
 
-    let guild_idExists = await Guild.findOne({ guild_id: req.body.email });
+    if (!req.body.guild_id) return self.raiseHttpError(res, `No guild_id provided`)
 
-    if (guild_idExists)
-      return self.raiseHttpError(res, `Guild already registered`);
+    let guild_idExists = await Guild.findOne({ guild_id: req.body.email })
+
+    if (guild_idExists) return self.raiseHttpError(res, `Guild already registered`)
 
     const newGuild = new Guild({
       guild_id: req.body.guild_id,
       guild_name: req.body.name
-    });
+    })
 
-    await newGuild.save().catch(err => res.send({err}))
-    await req.user.guilds.push(newGuild._id);
-    await req.user.save();
+    await newGuild.save().catch(err => res.send({ err }))
+    await req.user.guilds.push(newGuild._id)
+    await req.user.save()
 
     return await res.send({ ok: true, newGuild })
-
   }
-} 
+}
