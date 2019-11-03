0193c737c689e0da87e34e33b1beebc88d288411
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-03 13:24:48 -0300

    order attribute

--- src/controllers/song.js
+++ src/controllers/song.js
@@ -4,69 +4,71 @@ const Song = require('./../model/Song')
 const Playlist = require('./../model/Playlist')
 
 module.exports = {
-   async index(req, res) {
-      let playlists = await Playlist.find({
-         $and: [
-            { guild: { $in: req.user.guilds } },
-            { active: true }
-         ]
-      });
-      let songs = await Song.find({
-         $and: [
-            { playlist: { $in: playlists }},
-            { active: true },
-         ]
-      });
-      return res.send({ songs });
-   },
-   async get(req, res) {
-      let songs = await Song.get(req.params.playlist)
-      return res.send({ songs })
-   },
-   async store(req, res) {
-      let { youtube_link,
-            comment,
-            playlist_id,
-            playlist_name } = req.body;
+  async index(req, res) {
+    let playlists = await Playlist.find({
+      $and: [{ guild: { $in: req.user.guilds } }, { active: true }]
+    })
+    let songs = await Song.find({
+      $and: [{ playlist: { $in: playlists } }, { active: true }]
+    }).sort(['order', 1])
+    return res.send({ songs })
+  },
+  async get(req, res) {
+    let songs = await Song.get(req.params.playlist)
+    return res.send({ songs })
+  },
+  async store(req, res) {
+    let { youtube_link, comment, playlist_id, playlist_name, order } = req.body
 
-      if (!youtube_link)
-         return res.status(500).send({ error: 'No link provided' });
+    if (!youtube_link) return res.status(500).send({ error: 'No link provided' })
 
-      if (!playlist_id)
-         if (!playlist_id && !playlist_name)
-            return res.status(500).send({ error: 'No playlist name informed' });
+    if (!playlist_id)
+      if (!playlist_id && !playlist_name) return res.status(500).send({ error: 'No playlist name informed' })
 
+    const playlist = await Playlist.findOne({
+      $or: [{ _id: playlist_id }, { name: playlist_name }],
+      $and: [{ active: true }]
+    })
 
-      const playlist = await Playlist.findOne({
-         $or: [
-            { _id: playlist_id },
-            { name: playlist_name }
-         ],
-         $and: [
-            { active: true }
-         ]
-      });
+    if (!playlist || playlist.length < 1) return res.status(400).send({ error: 'Playlist does not exist' })
 
-      if (!playlist || playlist.length < 1)
-         return res.status(400).send({ error: "Playlist does not exist" });
+    const result = await Song({
+      youtube_link,
+      comment,
+      playlist: playlist,
+      order
+    })
 
-      const result = await Song({
-         youtube_link,
-         comment,
-         playlist: playlist
-      });
+    result.save().catch(err => console.log(err))
 
-      result.save().catch(err => console.log(err));
+    playlist.songs.push(result)
+    playlist.save().catch(err => console.log(err))
 
-      playlist.songs.push(result);
-      playlist.save().catch(err => console.log(err));
+    return res.send(result)
+  },
+  async update(req, res) {
+    let { _id, youtube_link, active, comment, order } = req.body
 
-      return res.send(result);
-   },
-   async remove(req, res) {
-      if (!req.body.music_id)
-         return res.status(400).send({ error: 'No id, no delete' })
-      let result = await Music.remove(req.body.music_id)
-      return res.send(result)
-   }
+    if (!_id) return res.status(400).send({ error: 'No id, no updates' })
+
+    let song = await Song.findOne({ _id })
+    if (!song) return res.status(400).send({ error: 'Song not found' })
+    if (youtube_link) song['youtube_link'] = youtube_link
+
+    // if must be either true or false
+    if (typeof active !== 'undefined') song.active = active
+
+    if (comment) song.comment = comment
+
+    if (order) song.order = order
+
+    await song.save().catch(err => console.log(err))
+    return res.send(song)
+  },
+  async remove(req, res) {
+    const { _id } = req.params
+    if (!_id) return res.status(400).send({ error: 'No id, no delete' })
+    const result = await Song.remove({ _id })
+    return res.send(result)
+  }
 }
