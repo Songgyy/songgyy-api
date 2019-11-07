b0bc923fd192a3e31906ea57e2757649ed58ca5c
Gabriel Petry <gabrielpetry@outlook.com>
2019-11-07 19:02:53 -0300

    order songs correctly

--- src/controllers/playlist.js
+++ src/controllers/playlist.js
@@ -20,9 +20,7 @@ module.exports = {
     const { playlist_name } = req.params
     const playlist = await Playlist.find({
       $and: [{ guild: { $in: req.user.guilds } }, { name: playlist_name }, { active: true }]
-    })
-      .populate('songs')
-      .sort(['order', 1])
+    }).populate({ path: 'songs', options: { sort: { order: 1 } } })
     return res.send({ ...playlist })
   },
   async store(req, res) {
